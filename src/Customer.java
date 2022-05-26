85326680719019c48d235770b3a25e2f2c67837d
2002chris <christian.geier02@t-online.de>
2022-05-26 20:09:49 +0200

    replace temp with query (frequentRenterPoints)

--- src/Customer.java
+++ src/Customer.java
@@ -3,7 +3,7 @@ import java.util.Vector;
 
 class Customer {
     private final String name;
-    private final Vector<Rental> rentals = new Vector<Rental>();
+    private final Vector<Rental> rentals = new Vector<>();
 
     public Customer(String name) {
         this.name = name;
@@ -27,21 +27,29 @@ class Customer {
         return result;
     }
 
+    private double getTotalFrequentRenterPoints(){
+        double result = 0;
+        Enumeration<Rental> enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental each = enum_rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
+
     public String statement() {
-        int frequentRenterPoints = 0;
         Enumeration<Rental> enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
             Rental aRental = enum_rentals.nextElement();
-            frequentRenterPoints += aRental.getFrequentRenterPoints();
             //show figures for this rental
             result += "\t" + aRental.getMovie().getTitle() + "\t" + "\t" + aRental.getDaysRented() + "\t" + aRental.getCharge() + "\n";
         }
         //add footer lines
         result += "Amount owed is " + getTotalAmount() + "\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
         return result;
     }
 
